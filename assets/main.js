0a5b813665d71300eb2bf3f655ed77c145c51ae2
Andrew Pucci <andrew@andrewpucci.com>
2018-06-04 19:50:07 -0400

    Draw circle on rink

--- assets/main.js
+++ assets/main.js
@@ -1,50 +1,74 @@
 (function() {
-  var svg = document.querySelector("svg");
-  var pt = svg.createSVGPoint();
+  const rink = document.getElementById("svg2457");
+  const pt = rink.createSVGPoint();
+  const xOffset = { NA: 1202 };
+  const yOffset = { NA: 512 };
+  const rinkType = "NA";
+  let tableData = [];
+  let shotCounter = 0;
+
   function cursorPoint(evt) {
     pt.x = evt.clientX;
     pt.y = evt.clientY;
-    return pt.matrixTransform(svg.getScreenCTM().inverse());
+    return pt.matrixTransform(rink.getScreenCTM().inverse());
   }
 
-  let tableData = [];
+  function drawCircle(elem, id, x, y) {
+    const ns = "http://www.w3.org/2000/svg";
+    const circle = document.createElementNS(ns, "circle");
+    circle.setAttributeNS(null, "id", id);
+    circle.setAttributeNS(null, "cx", x);
+    circle.setAttributeNS(null, "cy", y);
+    circle.setAttributeNS(null, "r", 25);
+    circle.setAttributeNS(null, "fill", "#f06");
+    elem.appendChild(circle);
+  }
 
-  svg.addEventListener(
+  function generateX(num, unitType) {
+    if (unitType === "cm") {
+      return Math.round((num - xOffset.NA) * 2.54, 1);
+    } else if (unitType === "in") {
+      return Math.round(num - xOffset.NA, 1);
+    } else if (unitType === "ft") {
+      return Math.round((num - xOffset.NA) / 12, 1);
+    }
+  }
+
+  function generateY(num, unitType) {
+    if (unitType === "cm") {
+      return Math.round(-(num - yOffset.NA) * 2.54, 1);
+    } else if (unitType === "in") {
+      return Math.round(-(num - yOffset.NA), 1);
+    } else if (unitType === "ft") {
+      return Math.round(-(num - yOffset.NA) / 12, 1);
+    }
+  }
+
+  rink.addEventListener(
     "mousedown",
     function(evt) {
-      const loc = cursorPoint(evt);
-      let coordinates = {};
-      const element = document.getElementById("unit-selector");
-      const unitType = element.options[element.selectedIndex].value;
-      if (unitType === "cm") {
-        coordinates = {
-          x: Math.round((loc.x - 1202) * 2.54, 1),
-          y: Math.round(-(loc.y - 512) * 2.54, 1)
-        };
-      } else if (unitType === "in") {
-        coordinates = {
-          x: Math.round(loc.x - 1202, 1),
-          y: Math.round(-(loc.y - 512), 1)
-        };
-      } else if (unitType === "ft") {
-        coordinates = {
-          x: Math.round((loc.x - 1202) / 12, 1),
-          y: Math.round(-(loc.y - 512) / 12, 1)
-        };
-      }
+      const shotID = `shot-${++shotCounter}`;
+      const shotLocation = cursorPoint(evt);
+      const unitSelector = document.getElementById("unit-selector");
+      const unitType = unitSelector.options[unitSelector.selectedIndex].value;
+      const coordinates = {
+        x: generateX(shotLocation.x, unitType),
+        y: generateY(shotLocation.y, unitType),
+        id: shotID
+      };
 
-      const newP = document.createElement("p");
-      const coords = document.createTextNode(
-        `${coordinates["x"]}, ${coordinates["y"]}`
-      );
-
+      // add coordinates to table
       tableData.unshift(coordinates);
       $("#coord-table").DataTable({
-        dom: "B<'clear'>rtip",
+        dom: "B<'clear'>rt<'clear'>i<'clear'>p",
         destroy: true,
         data: tableData,
         ordering: false,
-        columns: [{ title: "X", data: "x" }, { title: "Y", data: "y" }],
+        columns: [
+          { title: "X", data: "x" },
+          { title: "Y", data: "y" },
+          { title: "ID", data: "id", visible: false }
+        ],
         buttons: [
           {
             extend: "csvHtml5",
@@ -52,6 +76,9 @@
           }
         ]
       });
+
+      // draw a circle on the rink at shot location
+      drawCircle(rink, shotID, shotLocation.x, shotLocation.y);
     },
     false
   );
