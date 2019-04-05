05bb0970d34298371a83959c63258c442052000f
Andrew Pucci <andrewpucci@users.noreply.github.com>
2019-04-04 19:58:24 -0700

    Remove cachebuster

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,9 @@
-const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
-
 module.exports = function(config) {
-  // Layout aliases can make templates more portable
-  config.addLayoutAlias('default', 'layouts/base.njk');
-
   // Add some utility filters
   config.addFilter("squash", require("./src/filters/squash.js") );
 
   // minify the html output
   config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
-
-  // fingerprint css and js files
-  config.addPlugin(cacheBuster({
-    outputDirectory: 'dist',
-    createResourceHash(outputDirectoy, url, target) {
-      return Date.now();
-    }
-  }));
 
   // pass some assets right through
   config.addPassthroughCopy("./src/site/assets");
@@ -33,6 +20,5 @@ module.exports = function(config) {
     templateFormats : ["njk", "md"],
     htmlTemplateEngine : "njk",
     markdownTemplateEngine : "njk",
-    passthroughFileCopy: true,
   };
 };
